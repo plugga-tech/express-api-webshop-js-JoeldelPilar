e2c97d4ea01f5df1b5815b17d49fb0f75e1f808e
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-03-21 14:54:22 +0100

    :sparkles: add router function to get prod by id.

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,15 +2,31 @@ const express = require('express');
 const productModel = require('../models/product-model');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  // HÄMTA ALLA PRODUKTER
-  res.send('Hämtar alla produkter.');
+router.get('/', async function (req, res, next) {
+  const products = await productModel.find();
+  res.status(200).send(products);
 });
 
-router.get('/:id', function (req, res, next) {
-  // HÄMTA SPECIFIK PRODUKT
-  // ID PÅ PRODUKT
+router.get('/:id', async function (req, res, next) {
+
+  const product = req.params.id
+  const foundProduct = await productModel.findOne({ _id: product });
+
+  try {
+    if (foundProduct) {
+      res.status(200).send(foundProduct);
+    } else {
+      res.status(404).send('Could not find product.');
+    }
+  } catch (error) {
+    if (error.code === 'ECONNRESET') {
+      res.status(500).send('Connection was lost')
+    } else {
+      res.status(500).send(error);
+    }
+
+  }
+
 });
 
 router.post('/add', async function (req, res, next) {
