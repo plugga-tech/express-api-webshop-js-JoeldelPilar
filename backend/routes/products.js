e8ac53eadcb6b176526dba4e9110bafcdb252891
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-03-22 14:21:04 +0100

    :sparkle: add auth with token/API key

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,8 +3,15 @@ const productModel = require('../models/product-model');
 const router = express.Router();
 
 router.get('/', async function (req, res, next) {
-  const products = await productModel.find();
-  res.status(200).send(products);
+  try {
+    const products = await productModel.find();
+    if (products.length === 0) {
+      res.status(200).json('No products in database')
+    }
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(400).json(error, 'Could not load products, please try again later.')
+  }
 });
 
 router.get('/:id', async function (req, res, next) {
@@ -31,6 +38,11 @@ router.get('/:id', async function (req, res, next) {
 
 router.post('/add', async function (req, res) {
 
+  const token = req.body.token
+  if (token !== (process.env.API_KEY)) {
+    return res.status(401).json('Unauthorized request!');
+  }
+
   const existingProduct = await productModel.findOne({ name: req.body.name });
   const newProduct = {
     name: req.body.name,
@@ -43,18 +55,17 @@ router.post('/add', async function (req, res) {
   if (existingProduct) {
     const newLagerStatus = parseInt(existingProduct.lager) + parseInt(req.body.lager);
     await productModel.updateOne({ name: req.body.name }, { $set: { lager: newLagerStatus } });
-    res.status(201).json('New lager status is: ' + newLagerStatus);
+    res.status(200).json('New lager status is: ' + newLagerStatus);
   } else {
     try {
       const product = await productModel.create(newProduct).catch(err => {
         res.status(400).json(err, 'Could not add product, check product if product input values are correct')
       });
-      res.status(200).send(product);
+      res.status(201).send(product);
     } catch (error) {
       res.status(500).json(error.message);
     }
   }
 });
-
 
 module.exports = router;
